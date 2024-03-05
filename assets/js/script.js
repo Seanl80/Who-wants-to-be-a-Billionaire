7d1e3644313f7e55529a1779d1caaa47ed2a8c36
Seanl80 <seanlitt80@gmail.com>
2024-03-05 10:30:55 +0000

    startScreen function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
 let textElement = document.querySelector('.intro h1');
 textElement.style.setProperty('--animate-duration', '3s');
+let startScreen = document.querySelector('.start');
 let mainBodyElement = document.getElementById('main-body');
 
 textElement.classList.add('animate__animated', 'animate__zoomInDown', 'animate__slower');
 
 textElement.addEventListener('animationend', () => {
-  // Hide intro screen
   document.querySelector('.intro').style.display = 'none';
-  // Show main-body screen
-  mainBodyElement.style.display = 'block';
+  startScreen.style.display = 'block';
 });
 
+function startQuiz() {
+  startScreen.style.display = 'none';
+  mainBodyElement.style.display = 'block';
+}
 
 let question = document.getElementById('question');
 let choices = Array.from(document.getElementById('choice-text'));
